1ba88a547f7a61fc029553207c6c78d3619bd982
KyleKatarn <kylekatarnls@gmail.com>
2018-03-26 15:07:10 +0200

    Add some CLI feedback and handle file-server swap

--- src/Phug/PhugDevServer.php
+++ src/Phug/PhugDevServer.php
@@ -21,11 +21,51 @@ class PhugDevServer
 
     public function __construct($server, $file, $browserReloadPort)
     {
+        if (ctype_digit(strval($file)) && !file_exists($file)) {
+            $__server = $server;
+            $server = $file;
+            $file = $__server;
+        }
+
         $this->server = $server;
         $this->file = $file;
-        $this->browserReloadPort = $browserReloadPort;
+        $this->browserReloadPort = intval($browserReloadPort);
     }
 
+    /**
+     * @return int|string
+     */
+    public function getServer()
+    {
+        $serverPort = intval($this->server);
+        if ($serverPort) {
+            return "localhost:$serverPort";
+        }
+
+        return $this->server;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBrowserReloadPort()
+    {
+        return $this->browserReloadPort;
+    }
+
+    /**
+     * Start browser-reload and dev-server listening.
+     *
+     * @return bool
+     */
     public function listen()
     {
         $browserReloadPort = $this->browserReloadPort;
@@ -35,22 +75,26 @@ class PhugDevServer
         }
         $php = PHP_BINARY;
         $file = escapeshellarg($this->file);
-        $watcher = realpath(file_exists('vendor/bin/watcher') ? 'vendor/bin/watcher' : 'watcher');
+        $watcher = realpath(file_exists('vendor/phug/watcher/watcher') ? 'vendor/phug/watcher/watcher' : 'watcher');
         if (!$watcher) {
             throw new \RuntimeException('No watcher program found in the vendor/bin directory.');
         }
 
         // @codeCoverageIgnoreStart
         if (strtolower(substr(php_uname(), 0, 3)) === 'win') {
+            echo "Opening new window for browser-reload watching on port $browserReloadPort (or superior if not available).\n";
             pclose(popen("start $php $watcher --browser-reload=$browserReloadPort ".
                 "> watcher-output.log 2> watcher-error.log", 'r'));
+            echo "Listening $file on $server.\n";
             echo shell_exec("set BROWSER_RELOAD_PORT=$browserReloadPort && $php -S $server $file");
 
             return true;
         }
 
+        echo "Browser-reload watching listening on port $browserReloadPort (or superior if not available).\n";
         shell_exec("$php $watcher --browser-reload=$browserReloadPort ".
             "> watcher-output.log 2> watcher-error.log &");
+        echo "Listening $file on $server.\n";
         echo shell_exec("BROWSER_RELOAD_PORT=$browserReloadPort $php -S $server $file");
 
         return true;
---
+++ tests/Phug/PhugDevServerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Phug\Test;
+
+use Phug\PhugDevServer;
+
+/**
+ * @coversDefaultClass \Phug\PhugDevServer
+ */
+class PhugDevServerTest extends AbstractWatcherTestCase
+{
+    /**
+     * @covers ::<public>
+     */
+    public function testCreate()
+    {
+        $server = new PhugDevServer('localhost:9000', 'index.php', '3600');
+
+        self::assertSame('index.php', $server->getFile());
+        self::assertSame('localhost:9000', $server->getServer());
+        self::assertSame(3600, $server->getBrowserReloadPort());
+
+        $server = new PhugDevServer('7500', 'script', '3600');
+
+        self::assertSame('script', $server->getFile());
+        self::assertSame('localhost:7500', $server->getServer());
+        self::assertSame(3600, $server->getBrowserReloadPort());
+    }
+
+    /**
+     * @group i
+     * @covers ::<public>
+     */
+    public function testCreateArgumentsSwap()
+    {
+        $server = new PhugDevServer('script.php', '3600', '3000');
+
+        self::assertSame('script.php', $server->getFile());
+        self::assertSame('localhost:3600', $server->getServer());
+        self::assertSame(3000, $server->getBrowserReloadPort());
+    }
+}
